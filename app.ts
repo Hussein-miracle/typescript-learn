cf7fcdbbf63fe414468300bad1a7f3786752496e
Abdullahi Hussein <miraacle64@gmail.com>
2022-08-01 21:47:51 +0100

    feat:learning ts-types

--- app.ts
+++ app.ts
@@ -1,1 +1,21 @@
-console.log('Your code goes here...');
+type Person = {
+  name: string;
+  age: number;
+};
+
+const person= {
+  name: "Mirac",
+  age: 30,
+  hobbies:["Sports" , "Cooking" , "Exercising"],
+};
+
+
+
+let favActivities: Array<string>;
+
+favActivities = ["sports"];
+console.log(person.name);
+
+for(const hobby of person.hobbies){
+  console.log(hobby);
+}
---
+++ basics.ts
@@ -0,0 +1,20 @@
+function add(n1:number,n2:number,showRes:Boolean,phrase:string = ""){
+
+
+  if(showRes === true){
+    console.log(phrase + n1 + n2 );
+  }else{
+    return n1+n2;
+  }
+}
+
+
+const num1:number = 5;
+const num2:number = 2.8;
+
+const printResult:boolean = true;
+
+
+const res = add(num1,num2,printResult);
+
+// console.log(res);
